d8ec318e8d29ead7d3af4017eed19821034980fd
Fernando Alamillo <fernando@ongoney.com>
2020-11-18 11:35:49 +0000

    chore(setup) starting project

---
+++ src/fake/fake.stories.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Fake } from './fake';
+
+export default {
+  title: 'fake',
+};
+
+export const Primary = () => (
+  <div>
+    <Fake />
+  </div>
+);
---
+++ src/fake/fake.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Fake: React.FC = () => (
+  <div>
+    <h1>I'm the test component</h1>
+  </div>
+);
